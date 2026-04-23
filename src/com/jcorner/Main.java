88563bb0316888441939f3bddeac6e5fb1a18ce5
James <jamesthach64@gmail.com>
2026-04-23 07:04:56 -0400

    main class creation

---
+++ src/com/jcorner/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    /**
+     * J's Corner Restaurant Automation System.
+     * Team OffLimes - Intro to Software Engineering, Section W03 / SP26
+     *
+     * Populates the app w/ sample data and then inserts user at login screen
+     */
+    public static void main(String[] args) {
+
+    }
+}
