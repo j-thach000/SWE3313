92b5cbb0bf4e306ca6da0f28a7497f8c607dc5c9
James <jamesthach64@gmail.com>
2026-04-23 10:07:52 -0400

    added MenuItem's getters and setters

--- src/com/jcorner/model/MenuItem.java
+++ src/com/jcorner/model/MenuItem.java
@@ -17,4 +17,14 @@ public class MenuItem {
         category.getItems().add(this);
     }
 
+    public String getItemID() { return itemID; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { isAvailable = available; }
+    public MenuCategory getCategory() { return category; }
 }
