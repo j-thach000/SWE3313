ed2962a1ee60eb4e12f667ac9513801869882d0f
James <jamesthach64@gmail.com>
2026-04-23 11:46:45 -0400

    added InventoryItem's constructor

--- src/com/jcorner/model/InventoryItem.java
+++ src/com/jcorner/model/InventoryItem.java
@@ -9,4 +9,13 @@ public class InventoryItem {
     private String unit;              // "lbs", "ct", "heads", "boxes"
     private LocalDateTime lastChecked;
     private int lowThreshold;         // below this => LOW
+
+    public InventoryItem(String itemID, String name, int quantity, String unit, int lowThreshold) {
+        this.itemID = itemID;
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.lowThreshold = lowThreshold;
+        this.lastChecked = LocalDateTime.now();
+    }
 }
