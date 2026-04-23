4345bdb9a447a372f7a4c8821e9b1c52799f3809
James <jamesthach64@gmail.com>
2026-04-23 10:01:59 -0400

    adjusted field name

---
+++ src/com/jcorner/model/MenuItem.java
@@ -0,0 +1,11 @@
+package com.jcorner.model;
+
+public class MenuItem {
+    private final String itemID;
+    private String name;
+    private double price;
+    private String description;
+    private boolean isAvailable = true;
+    private MenuCategory category;
+
+}
