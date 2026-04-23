b3478afb4444a6789a909ca622c9bf4ff4151d20
James <jamesthach64@gmail.com>
2026-04-23 09:55:51 -0400

    MenuCategory fields created

---
+++ src/com/jcorner/model/MenuCategory.java
@@ -0,0 +1,16 @@
+package com.jcorner.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuCategory {
+    private final String categoryID;
+    private String name;
+    private double price;
+    private String description;
+    private boolean isAvailable = true;
+    private MenuCategory category;
+
+    
+
+}
