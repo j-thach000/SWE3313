b2ea6603191391f32c51c75fad64d5ed3014ce7a
James <jamesthach64@gmail.com>
2026-04-23 08:42:55 -0400

    created ReportType classifications

---
+++ src/com/jcorner/model/ReportType.java
@@ -0,0 +1,10 @@
+package com.jcorner.model;
+
+public enum ReportType {
+    DAILY_SALES,
+    REVENUE_BY_ITEM,
+    ITEM_POPULARITY,
+    TURNAROUND_TIME,
+    PREP_TIME,
+    PERSONNEL_EFFICIENCY
+}
